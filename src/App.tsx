803608912ccfade373bddb2e94a7b9512eb51f97
suyash shakya <suyashshakya.h0p3@gmail.com>
2022-07-06 07:43:45 +0545

    fix: redux changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,19 @@ import Events from "./components/Events";
 import EventForm from "./components/EventForm";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./redux/store";
+import { fetchEvents } from "./redux/eventsSlice";
 
 const App = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  React.useEffect(() => {
+    dispatch(fetchEvents());
+  }, []);
+  const eventData = useSelector((state: RootState) => state);
+  const events = eventData?.events?.event?.[0];
+  console.log("events", events);
+
   return (
     <Box minH="100%">
       <Router>
---
+++ src/redux/eventsSlice.ts
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import * as api from "../api";
+
+export interface EventState {
+  event: Array<object>;
+}
+
+const initialState: EventState = {
+  event: [],
+};
+
+export const fetchEvents = createAsyncThunk("events", async () => {
+  const { data } = await api.fetchEvents();
+  console.log("gandu", data);
+  return data;
+});
+
+export const eventsSlice = createSlice({
+  name: "events",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(fetchEvents.fulfilled, (state, action) => {
+      // Add user to the state array
+      state.event.pop();
+      state.event.push(action.payload);
+    });
+  },
+});
+
+// Action creators are generated for each case reducer function
+// export const { fetchEvents } = eventsSlice.actions;
+
+export default eventsSlice.reducer;
